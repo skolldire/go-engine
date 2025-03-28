d24dc4fb22df80d8f01b3ceebc4fd8d1d9592f7c
Fredy Hernández <hfredy717@outlook.com>
2025-03-28 16:46:04 -0500

    :construction: improve initial version

--- pkg/app/router/entity.go
+++ pkg/app/router/entity.go
@@ -33,20 +33,20 @@ type App struct {
 }
 
 type Config struct {
-	Port             string        `mapstructure:"port"`
-	Name             string        `mapstructure:"name"`
-	ReadTimeout      time.Duration `mapstructure:"read_timeout"`
-	WriteTimeout     time.Duration `mapstructure:"write_timeout"`
-	IdleTimeout      time.Duration `mapstructure:"idle_timeout"`
-	ShutdownTimeout  time.Duration `mapstructure:"shutdown_timeout"`
-	EnableCORS       bool          `mapstructure:"enable_cors"`
-	AllowOrigins     []string      `mapstructure:"allow_origins"`
-	AllowMethods     []string      `mapstructure:"allow_methods"`
-	AllowHeaders     []string      `mapstructure:"allow_headers"`
-	ExposedHeaders   []string      `mapstructure:"exposed_headers"`
-	AllowCredentials bool          `mapstructure:"allow_credentials"`
-	AllowMaxAge      int           `mapstructure:"allow_max_age"`
-	TrustedProxies   []string      `mapstructure:"trusted_proxies"`
+	Port             string        `mapstructure:"port" json:"port"`
+	Name             string        `mapstructure:"name" json:"name"`
+	ReadTimeout      time.Duration `mapstructure:"read_timeout" json:"read_timeout"`
+	WriteTimeout     time.Duration `mapstructure:"write_timeout" json:"write_timeout"`
+	IdleTimeout      time.Duration `mapstructure:"idle_timeout" json:"idle_timeout"`
+	ShutdownTimeout  time.Duration `mapstructure:"shutdown_timeout" json:"shutdown_timeout"`
+	EnableCORS       bool          `mapstructure:"enable_cors" json:"enable_cors"`
+	AllowOrigins     []string      `mapstructure:"allow_origins" json:"allow_origins"`
+	AllowMethods     []string      `mapstructure:"allow_methods" json:"allow_methods"`
+	AllowHeaders     []string      `mapstructure:"allow_headers" json:"allow_headers"`
+	ExposedHeaders   []string      `mapstructure:"exposed_headers" json:"exposed_headers"`
+	AllowCredentials bool          `mapstructure:"allow_credentials" json:"allow_credentials"`
+	AllowMaxAge      int           `mapstructure:"allow_max_age" json:"allow_max_age"`
+	TrustedProxies   []string      `mapstructure:"trusted_proxies" json:"trusted_proxies"`
 }
 
 type RouterOption func(*App)
